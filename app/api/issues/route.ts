0de990f7d389967ff17d73cd30ac59d438009bec
Suhadip Kundu <subhadipkundu1000@gmail.com>
2024-02-19 20:34:21 +0530

    create create-issue form, make client-validation, and server validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,18 +1,15 @@
 
 import { NextRequest, NextResponse } from "next/server";
-
-import { z } from "zod";
+import { createIssueSchema } from "../../validation"
 import prisma from "../../../prisma/client"
-const issueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1).max(255)
-})
+
 
 export async function POST(request: NextRequest) {
 
     const body = await request.json();
 
-    const validation = issueSchema.safeParse(body);
+    const validation = createIssueSchema.safeParse(body);
+    console.log(validation);
 
     if (!validation.success) {
         return NextResponse.json(validation.error.errors, { status: 400 });
