885e753fb75df88330c537a1b3243ad435bc6e6c
Suhadip Kundu <subhadipkundu1000@gmail.com>
2024-02-20 20:45:05 +0530

    built edit details page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,12 @@
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
 import  Prisma  from '../../../prisma/client'
 import React from 'react'
 import Markdown from 'react-markdown'
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import delay from 'delay'
+import { FaRegEdit } from "react-icons/fa";
+import EditIssueButton from './EditIssueButton'
+import IssueDetails from './IssueDetails'
 interface Props{
     params:{
         id:string
@@ -18,25 +21,15 @@ async function IssueDetailsPage({params:{id}}: Props) {
     })
 
   return (
-    <>
-    {issueDetails && 
-    <div className='p-5 mt-10 prose'>
-        <Heading>
-            {issueDetails?.title}
-        </Heading>
-        
-        <Flex gap={'4'} my={'5'}>
-            <IssueStatusBadge status={issueDetails.status}/>
-            <Text>{issueDetails.createdAt.toDateString()} </Text>
-        </Flex>
-        <Card>
-          <Markdown>{issueDetails.description}</Markdown>
-        </Card>
-        
-      
-    </div>
-  }
-  </>
+    <Grid columns={{initial:"1", md:"2"}} className='p-5 mt-10 prose min-w-full gap-5'>
+        <Box className='min-w-full'> 
+             { issueDetails  &&  <IssueDetails issueDetails={issueDetails}/> }
+        </Box>
+        <Box>
+            <EditIssueButton issueid={issueDetails!.id}/>
+        </Box>
+    </Grid> 
+   
   )
 }
 
