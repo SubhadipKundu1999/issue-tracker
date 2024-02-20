1ee81cfd2b6a4e4cd71006db088c7fd7fc3f2913
Suhadip Kundu <subhadipkundu1000@gmail.com>
2024-02-20 10:50:10 +0530

    built issue details page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import {Table } from '@radix-ui/themes';
 
-import Link from 'next/link';
+import Link from '../components/Link';
 import Prisma  from "../../prisma/client"
 import IssueStatusBadge from '../components/issueStatusBadge';
 import delay from 'delay'
@@ -32,7 +32,7 @@ async function  page() {
     {issues && issues.map((issue)=>
 
 <Table.Row key ={issue.id}>
-<Table.Cell>{issue.title} <div className='block md:hidden'><IssueStatusBadge status={issue.status}/></div></Table.Cell>
+<Table.Cell><Link href={`/issues/${issue.id}`}>{issue.title} </Link><div className='block md:hidden'><IssueStatusBadge status={issue.status}/></div></Table.Cell>
 <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status}/></Table.Cell>
 <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
 </Table.Row>
